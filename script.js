c7eeb81a75df578f877240fe72d4ca752f9efc6c
Matthew Boeving <mboeving1@gmail.com>
2021-06-09 22:18:41 -0400

    Javascript exercise 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let actualTemp = 100;
+let desiredTemp = 75;
+
+if (actualTemp<desiredTemp){
+    console.log("Run Heat");
+} else if(actualTemp>desiredTemp){
+    console.log("Run A/C");
+} else{
+    console.log("Standby");
+}
